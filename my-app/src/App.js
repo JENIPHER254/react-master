60831d1371b64eaa1871dad00d45a2d8d38ef8d2
kaitli <karanjajames904@gmail.com>
2025-10-31 19:27:49 +0300

    feat: routing about and head pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import Header from './components/common/heading/Header'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div>
-      HELLO WORLD
-    </div>
+    <>
+     
+      <Router>
+         <Header />
+         <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+         </Switch>
+      </Router>
+    </>
   )
 }
 
