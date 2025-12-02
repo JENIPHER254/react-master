f6c98c7655ff68a3dae0538e249853f48845bd10
jenipher <jeniperkuki@gmail.com>
2025-12-02 12:27:22 +0300

    feat: accessing and displaying emojis in react

--- my_app_js/src/DestracturingProps1.jsx
+++ my_app_js/src/DestracturingProps1.jsx
@@ -1,5 +1,5 @@
 function Greeting1(props) {
     const { name , message} = props;
-    return <p>{message} {name}!</p>;
+    return <p>{message} {name}!😊</p>;//use win + . to access emojis
 }
 export default Greeting1;
